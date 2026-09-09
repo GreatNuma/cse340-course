f6ab5b5dde63ef648c54352723fe0b23ee271bcd
GreatNuma <gnuma@byupathway.edu>
2026-09-09 19:57:37 +0100

    new

--- server.js
+++ server.js
@@ -1,55 +1,42 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+ 
 // Define the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
-
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
-
+ 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ 
 const app = express();
-
-/**
-  * Configure Express middleware
-  */
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set EJS as templating engine
-app.set('view engine', 'ejs');
-
-// Tell Express where to find your templates
-// app.set('views', path.join(__dirname, 'views'));
-
-/**
- * Routes
- */
-app.get('/', async (req, res) => {
-  const title = 'Home';
-  res.render('home', { title });
+ 
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+ 
+app.get("/", async (req, res) => {
+	const title = "Home";
+	res.render("home", { title });
 });
-
-app.get('/organizations', async (req, res) => {
-  const title = 'Our Partner Organizations';
-  res.render('organizations', { title });
+ 
+app.get("/organizations", async (req, res) => {
+	const title = "Our partner organizations";
+	res.render("organizations", { title });
 });
-
-app.get('/projects', async (req, res) => {
-  const title = 'Service Projects';
-  res.render('projects', { title });
+ 
+app.get("/categories", async (req, res) => {
+	const title = "Service Categories";
+	res.render("categories", { title });
 });
-
-app.get('/categories', async (req, res) => {
-  const title = 'Service Project Categories';
-  res.render('categories', { title });
+ 
+app.get("/projects", async (req, res) => {
+	const title = "Service Projects";
+	res.render("projects", { title });
 });
-
+ 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
-  console.log(`Environment: ${NODE_ENV}`);
+	console.log(`Server is running at http://127.0.0.1:${PORT}`);
+	console.log(`Environment: ${NODE_ENV}`);
 });
