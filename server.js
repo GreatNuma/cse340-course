cd8dbeff140869288a855831c820d53e0a02f187
GreatNuma <gnuma@byupathway.edu>
2026-09-16 02:17:41 +0100

    Query database for organizations and render dynamically in EJS

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
@@ -21,9 +23,11 @@ app.get("/", async (req, res) => {
 	res.render("home", { title });
 });
 
-app.get("/organizations", async (req, res) => {
-	const title = "Our partner organizations";
-	res.render("organizations", { title });
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get("/categories", async (req, res) => {
@@ -36,7 +40,12 @@ app.get("/projects", async (req, res) => {
 	res.render("projects", { title });
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running at http://127.0.0.1:${PORT}`);
-	console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+	try {
+		await testConnection();
+		console.log(`Server is running at http://127.0.0.1:${PORT}`);
+		console.log(`Environment: ${NODE_ENV}`);
+	} catch (error) {
+		console.error('Error connecting to the database:', error);
+	}
 });
